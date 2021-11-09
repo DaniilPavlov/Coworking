98e506ddf31327fee910cb3163430457b1776b08
DaniilPavlov <cool.sharagin@mail.ru>
2021-11-09 21:33:12 +0300

    map moves to users location in start. Location permission and enabling TODO

--- lib/screens/map/map.dart
+++ lib/screens/map/map.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:coworking/models/account.dart';
 import 'package:coworking/navigation/main_navigation.dart';
+import 'package:coworking/screens/login/logo_decoration.dart';
 import 'package:coworking/screens/map/bottom_nav_bar.dart';
 import 'package:coworking/services/database_pin.dart';
 import 'package:coworking/models/pin.dart';
@@ -11,15 +12,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:io';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/foundation.dart';
-import 'package:permission_handler/permission_handler.dart';
 
-import 'package:coworking/services/sign_in.dart';
 import 'package:coworking/screens/menu/menu_drawer.dart';
-import 'package:coworking/screens/map/map_search.dart';
 import 'package:coworking/screens/map/pin/create_pin.dart';
 
 class MapPage extends StatefulWidget {
@@ -55,11 +54,6 @@ class MapPageState extends State<MapPage> with SingleTickerProviderStateMixin {
   late FloatingActionButton fabConfirmPin;
   late FloatingActionButton currentFab;
 
-  /// ПОПРОБОВАТЬ ИСПОЛЬЗОВАТЬ, ЧТОБЫ НЕ ЗАПРАШИВАТЬ АВТОРСТВО ИЗ БАЗЫ КАЖДЫЙ РАЗ
-  // для пользователя
-  late User _user;
-  late Account _account;
-
   // открывыем дроуер снизу вверх, для центральной круглой кнопки
   void openDrawer() {
     setState(() {
@@ -88,12 +82,6 @@ class MapPageState extends State<MapPage> with SingleTickerProviderStateMixin {
 
     Navigator.of(context)
         .pushNamed(MainNavigationRouteNames.pinDetails, arguments: pin);
-
-    // showModalBottomSheet(
-    //   isScrollControlled: true,
-    //   context: context,
-    //   builder: (_) => PinInfo(pin),
-    // );
   }
 
   void barHeightChange(double height) {
@@ -140,6 +128,69 @@ class MapPageState extends State<MapPage> with SingleTickerProviderStateMixin {
     });
   }
 
+  Position currentPosition = const Position(
+      longitude: 30.359357,
+      latitude: 69.933895,
+      accuracy: 0.0,
+      altitude: 0.0,
+      heading: 0.0,
+      speed: 0.0,
+      speedAccuracy: 0.0,
+      timestamp: null);
+
+  bool locationEnabled = false;
+
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  Future<Position> _determinePosition() async {
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    locationEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!locationEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    currentPosition = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    print("LONG" + currentPosition.latitude.toString());
+    setState(() {
+      currentPosition;
+    });
+    currentMapPosition = CameraPosition(
+        target: LatLng(currentPosition.latitude, currentPosition.longitude),
+        zoom: MapPage.kDefaultZoom);
+    return currentPosition;
+  }
+
+  Future? getLocation;
+
   @override
   void initState() {
     super.initState();
@@ -150,11 +201,11 @@ class MapPageState extends State<MapPage> with SingleTickerProviderStateMixin {
     showDrawer = false;
     mapOverlap = EdgeInsets.zero;
 
-    currentMapPosition = ((widget.currentMapPosition == null)
-        ? const CameraPosition(
-            target: LatLng(59.933895, 30.359357), zoom: MapPage.kDefaultZoom)
-        : widget.currentMapPosition)!;
+    currentMapPosition = CameraPosition(
+        target: LatLng(currentPosition.latitude, currentPosition.longitude),
+        zoom: MapPage.kDefaultZoom);
 
+    getLocation = _determinePosition();
     pinFormKey = GlobalKey<CreatePinState>();
 
     fabAddPin = FloatingActionButton(
@@ -180,107 +231,99 @@ class MapPageState extends State<MapPage> with SingleTickerProviderStateMixin {
 
     currentFab = fabAddPin;
 
-    var user = SignIn.auth.currentUser;
-    Account account = Account.fromFirebaseUser(user!);
-    setState(() {
-      _user = user;
-      _account = account;
-    });
-
     queryPins();
 
-    // if (Platform.isIOS) {
-    //   _firebaseMessaging
-    //       .requestNotificationPermissions(IosNotificationSettings());
-    //   _firebaseMessaging.onIosSettingsRegistered.listen((event) {
-    //     print("IOS Registered");
-    //   });
-    // }
-
-    ///Добавил для уведомлений, нужно добавить алерт диалоги, если мы находимся
-    ///в приложении (уведомления приходят только в бэкграунде)
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       print("onMessage: $message");
     });
 
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
       print("onLaunch: $message");
-      Navigator.of(context)
-                    .pushNamed(MainNavigationRouteNames.meetingsScreen);
+      Navigator.of(context).pushNamed(MainNavigationRouteNames.meetingsScreen);
     });
   }
-  // FirebaseMessaging.onBackgroundMessage((RemoteMessage message) {
-  //   print("onBackgroundMessage: $message");
-  // });
 
   ///WIDGET BUILD
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-        onWillPop: () {
-          showDialog(
-              context: context,
-              builder: (context) => AlertDialog(
-                      title: const Text(
-                        "Вы действительно хотите выйти?",
-                        style: TextStyle(color: Colors.orange),
-                      ),
-                      actions: <Widget>[
-                        ElevatedButton(
-                            child: const Text(
-                              "Да",
-                              style: TextStyle(color: Colors.white),
+    print("REBUILD " + currentPosition.longitude.toString());
+    return FutureBuilder(
+        future: getLocation,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return WillPopScope(
+              onWillPop: () {
+                showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                            title: const Text(
+                              "Вы действительно хотите выйти?",
+                              style: TextStyle(color: Colors.orange),
                             ),
-                            style: ButtonStyle(
-                              backgroundColor:
-                                  MaterialStateProperty.all(Colors.orange),
-                            ),
-                            onPressed: () {
-                              exit(0);
-                            }),
-                        const SizedBox(
-                          width: 100,
-                        ),
-                        TextButton(
-                          child: const Text("Нет",
-                              style: TextStyle(color: Colors.white)),
-                          style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all(Colors.orange),
-                          ),
-                          onPressed: () => Navigator.pop(context, false),
-                        ),
-                      ]));
-          return true as Future<bool>;
-        },
-        child: Scaffold(
-          body: MapBody(
-            mapMoveCallback: (value) => currentMapPosition = value,
-            initialPosition: currentMapPosition,
-            mapOverlap: mapOverlap,
-            drawerHeight: drawerHeight,
-            pins: pins,
-            pinAnimation: drawerAnimator,
-            pinsStream: pinsStream,
-          ),
-          drawer: const MenuDrawer(),
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerDocked,
-          floatingActionButton: currentFab,
-          resizeToAvoidBottomInset: false,
-          // сделанно для того, чтобы при открытии клавиатуры карта не изменяла размер
-          extendBody: true,
-          // сверху над картой помещаем боттом нав бар
-          bottomNavigationBar: BottomBar(
-            pinFormKey,
-            closeDrawer,
-            mapOverlap == EdgeInsets.zero ? barHeightChange : (_) {},
-            drawerHeight,
-            drawerAnimator,
-            showDrawer,
-            updateMapPosition,
-          ),
-        ));
+                            actions: <Widget>[
+                              ElevatedButton(
+                                  child: const Text(
+                                    "Да",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all(
+                                        Colors.orange),
+                                  ),
+                                  onPressed: () {
+                                    exit(0);
+                                  }),
+                              const SizedBox(
+                                width: 100,
+                              ),
+                              TextButton(
+                                child: const Text("Нет",
+                                    style: TextStyle(color: Colors.white)),
+                                style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all(Colors.orange),
+                                ),
+                                onPressed: () => Navigator.pop(context, false),
+                              ),
+                            ]));
+                return true as Future<bool>;
+              },
+              child: Scaffold(
+                body: MapBody(
+                  mapMoveCallback: (value) => currentMapPosition = value,
+                  initialPosition: currentMapPosition,
+                  locationEnabled: locationEnabled,
+                  mapOverlap: mapOverlap,
+                  drawerHeight: drawerHeight,
+                  pins: pins,
+                  pinAnimation: drawerAnimator,
+                  pinsStream: pinsStream,
+                ),
+                drawer: const MenuDrawer(),
+                floatingActionButtonLocation:
+                    FloatingActionButtonLocation.centerDocked,
+                floatingActionButton: currentFab,
+                resizeToAvoidBottomInset: false,
+                // сделанно для того, чтобы при открытии клавиатуры карта не изменяла размер
+                extendBody: true,
+                // сверху над картой помещаем боттом нав бар
+                bottomNavigationBar: BottomBar(
+                  pinFormKey,
+                  closeDrawer,
+                  mapOverlap == EdgeInsets.zero ? barHeightChange : (_) {},
+                  drawerHeight,
+                  drawerAnimator,
+                  showDrawer,
+                  updateMapPosition,
+                ),
+              ),
+            );
+          } else {
+            return const LogoDecoration(
+              child: Center(child: CircularProgressIndicator()),
+            );
+          }
+        });
   }
 
   @override
@@ -295,6 +338,7 @@ class MapBody extends StatefulWidget {
     Key? key,
     required this.mapMoveCallback,
     required this.initialPosition,
+    required this.locationEnabled,
     required this.mapOverlap,
     required this.drawerHeight,
     required this.pins,
@@ -304,6 +348,7 @@ class MapBody extends StatefulWidget {
 
   final Function(CameraPosition) mapMoveCallback;
   final CameraPosition initialPosition;
+  final bool locationEnabled;
   final EdgeInsets mapOverlap;
   final double drawerHeight;
 
@@ -317,56 +362,51 @@ class MapBody extends StatefulWidget {
 }
 
 class MapBodyState extends State<MapBody> with WidgetsBindingObserver {
-  static const CameraPosition startPosition = CameraPosition(
-      target: LatLng(59.933895, 30.359357), zoom: MapPage.kDefaultZoom);
-
-  bool locationEnabled = false;
-
   // Обновляем карту и определяем пермишены. Однако, если человек установил
   // режим больше не спрашивать - не спросим
-  void monitorLocationPerm() async {
-    ServiceStatus? currentServiceStatus, oldServiceStatus;
+  // void monitorLocationPerm() async {
+  //   ServiceStatus? currentServiceStatus, oldServiceStatus;
 
-    while (true) {
-      oldServiceStatus = currentServiceStatus;
-      currentServiceStatus = (await Permission.location.serviceStatus);
+  //   while (true) {
+  //     oldServiceStatus = currentServiceStatus;
+  //     currentServiceStatus = (await Permission.location.serviceStatus);
 
-      // проверяем пермишены только если изменился статус сервиса
-      if (currentServiceStatus == oldServiceStatus) continue;
+  //     // проверяем пермишены только если изменился статус сервиса
+  //     if (currentServiceStatus == oldServiceStatus) continue;
 
-      // если джипиэс выключен - локация недоступна
-      // кнопка *наведись на нас* пропадает
-      if (currentServiceStatus == ServiceStatus.disabled) {
-        setState(() {
-          locationEnabled = false;
-        });
-        continue;
-      }
+  //     // если джипиэс выключен - локация недоступна
+  //     // кнопка *наведись на нас* пропадает
+  //     if (currentServiceStatus == ServiceStatus.disabled) {
+  //       setState(() {
+  //         locationEnabled = false;
+  //       });
+  //       continue;
+  //     }
 
-      // джипиэс включили, проверяем разрешения
-      PermissionStatus permissionStatus = await Permission.location.status;
+  //     // джипиэс включили, проверяем разрешения
+  //     PermissionStatus permissionStatus = await Permission.location.status;
 
-      if (permissionStatus == PermissionStatus.denied ||
-          permissionStatus == PermissionStatus.permanentlyDenied) {
-        permissionStatus = (await Permission.location.request());
-      }
+  //     if (permissionStatus == PermissionStatus.denied ||
+  //         permissionStatus == PermissionStatus.permanentlyDenied) {
+  //       permissionStatus = (await Permission.location.request());
+  //     }
 
-      setState(() {
-        if (permissionStatus == PermissionStatus.denied ||
-            permissionStatus == PermissionStatus.permanentlyDenied) {
-          locationEnabled = false;
-        } else if (permissionStatus == PermissionStatus.granted) {
-          locationEnabled = true;
-        }
-      });
-    }
-  }
+  //     setState(() {
+  //       if (permissionStatus == PermissionStatus.denied ||
+  //           permissionStatus == PermissionStatus.permanentlyDenied) {
+  //         locationEnabled = false;
+  //       } else if (permissionStatus == PermissionStatus.granted) {
+  //         locationEnabled = true;
+  //       }
+  //     });
+  //   }
+  // }
 
   //начальное состояние, проверяем пермишены
   @override
   void initState() {
     super.initState();
-    monitorLocationPerm();
+    // monitorLocationPerm();
     WidgetsBinding.instance!.addObserver(this);
   }
 
@@ -397,14 +437,15 @@ class MapBodyState extends State<MapBody> with WidgetsBindingObserver {
               children: <Widget>[
                 GoogleMap(
                   initialCameraPosition: widget.initialPosition,
+
                   padding: widget.mapOverlap +
                       EdgeInsets.only(
                           bottom:
                               widget.drawerHeight * widget.pinAnimation.value),
                   // поднимаем +- и надпись гугл
                   markers: markers,
-                  myLocationEnabled: locationEnabled,
-                  myLocationButtonEnabled: locationEnabled,
+                  myLocationEnabled: widget.locationEnabled,
+                  myLocationButtonEnabled: widget.locationEnabled,
                   onCameraMove: widget.mapMoveCallback,
                   gestureRecognizers: {}
                     ..add(Factory<PanGestureRecognizer>(
@@ -448,4 +489,3 @@ class MapBodyState extends State<MapBody> with WidgetsBindingObserver {
     );
   }
 }
-
